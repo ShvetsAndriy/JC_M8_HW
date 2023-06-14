882938cb3c98dbb02144449f6cc99cc605d79fd1
Andriy Shvets <andriy2.shvets@gmail.com>
2023-06-14 18:59:22 +0300

    second commit - changed main class

--- src/MainInfoShape.java
+++ src/MainInfoShape.java
@@ -2,11 +2,19 @@ import shapesclasses.*;
 
 class MainInfoShape {
     public static void main(String[] args) {
+        Shape[] shapes = {new Line(), new Square(), new Circle(), new Trapezoid(), new Triangle()};
+        GetNameOfShape namePrinter = new GetNameOfShape();
+        for (Shape shape : shapes) {
+            namePrinter.printName(shape);
+        }
+
+/*
         new GetNameOfShape().printName(new Line());
         new GetNameOfShape().printName(new Square());
         new GetNameOfShape().printName(new Circle());
         new GetNameOfShape().printName(new Trapezoid());
         new GetNameOfShape().printName(new Triangle());
+*/
     }
 }
 
